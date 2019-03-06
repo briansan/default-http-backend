7727d9258b66dfe1c923d020c6a8f3ebe1d6339d
Brian Kim <brian@tigera.io>
2019-03-06 14:51:15 -0800

    add ability to crash the server

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/labstack/echo"
 )
 
@@ -8,6 +10,13 @@ func main() {
 	e := echo.New()
 	e.GET("/healthz", func(c echo.Context) error {
 		return c.NoContent(200)
+	})
+	e.GET("/crash", func(c echo.Context) error {
+		go func() {
+			time.Sleep(5 * time.Second)
+			panic("crashed")
+		}()
+		return c.NoContent(500)
 	})
 	e.Start(":8000")
 }
